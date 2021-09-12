9ca1b88e732d6dc07ec9b12911b49c2aeb25c3a3
Ed Salinas <edsalinas7b@yahoo.com>
2021-09-13 04:31:52 +0800

    Mailer, env, seed, dummy scaffold for template

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,4 +49,21 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.delivery_method = :smtp
+  host = 'localhost' #replace with your own url
+  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV['email'],
+    :password             => ENV['email_password'],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_05_230022) do
+ActiveRecord::Schema.define(version: 2021_09_12_194049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -49,6 +49,13 @@ ActiveRecord::Schema.define(version: 2021_09_05_230022) do
     t.float "pay_rate"
     t.float "total_earned"
     t.string "employment_contract"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
