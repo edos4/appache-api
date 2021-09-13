9693f81ccc39b2b91b1beec631fc90f7e5b5abf9
Ed Salinas <edsalinas7b@yahoo.com>
2021-09-13 11:56:52 +0800

    Disable redis

--- config/initializers/jwt_denylist.rb
+++ config/initializers/jwt_denylist.rb
@@ -2,11 +2,12 @@ class JwtDenylist < ApplicationRecord
   include Devise::JWT::RevocationStrategies::Denylist
 
   def self.jwt_revoked?(payload, user)
-    $redis.exists?("user_denylist:#{user.id}:#{payload['jti']}")
+    #$redis.exists?("user_denylist:#{user.id}:#{payload['jti']}")
+    false
   end
 
   def self.revoke_jwt(payload, user)
-    expiration = payload['exp'] - payload['iat']
-    $redis.setex("user_denylist:#{user.id}:#{payload['jti']}", expiration, payload['jti'])
+    #expiration = payload['exp'] - payload['iat']
+    #$redis.setex("user_denylist:#{user.id}:#{payload['jti']}", expiration, payload['jti'])
   end
 end
