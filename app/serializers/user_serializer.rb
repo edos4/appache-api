3ca5160660b77233fc94ba9e7368db7425c88748
Ed Salinas <edsalinas7b@yahoo.com>
2021-10-25 10:05:14 +0800

    Display perms upon login

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,22 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :created_at, :updated_at, :role
+  attributes :id, :email, :created_at, :updated_at, :role, :permissions
 
   def role
     (object.staff.role rescue nil)
+  end
+
+  def permissions
+    classes = [Studio, Campaign]     
+
+    permissions = { }                             
+
+    classes.each do |clazz|                       
+      policy =  Pundit.policy(current_user, clazz)      
+      policy.public_methods(false).sort.each do |m|      
+        result = policy.send m                   
+        permissions["#{clazz}.#{m}"] = result if m != :record     
+      end
+    end 
+    permissions
   end
 end
